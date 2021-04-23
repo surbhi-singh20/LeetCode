404c6615be235884f8b77b8a88cd20317da57543
surbhi-singh20 <61513342+surbhi-singh20@users.noreply.github.com>
2021-04-23 19:01:32 +0530

    Time: 1 ms (95.91%), Space: 39.4 MB (7.62%) - LeetHub

---
+++ car-pooling/car-pooling.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] timestamp = new int[1001];
+        for(int[] t: trips){
+            timestamp[t[1]] += t[0];
+            timestamp[t[2]] -= t[0];
+        }
+        
+        for(int val: timestamp){
+            capacity -= val;
+            if(capacity < 0) return false;
+        }
+        return true;
+    }
+}
